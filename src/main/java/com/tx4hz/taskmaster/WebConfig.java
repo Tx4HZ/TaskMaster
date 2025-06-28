6ffadc973bfa849e6308c09890dbc45925539e54
tx4hz <karagodin.valeryn@gmail.com>
2025-06-28 13:18:23 +0500

    Backup before restructuring

---
+++ src/main/java/com/tx4hz/taskmaster/WebConfig.java
@@ -0,0 +1,11 @@
+package com.tx4hz.taskmaster;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig  implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:8081");
+    }
+}
